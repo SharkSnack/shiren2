decfbe25d755da76d4d0ecf1892212190187aa15
SharkSnack <sharksnack2@gmail.com>
2020-02-18 14:35:03 -0600

    see, it was already a bad idea

--- js/item.js
+++ js/item.js
@@ -18,9 +18,6 @@ try {
 $(document).ready(function() {
   // load some common HTML
   try {
-    $("#header").load("../../html/header.html", function() {
-      console.log('info - loaded header');
-    });
     $("#sidebar").load("../../html/sidebar.html", function() {
       console.log('info - loaded sidebar');
     });
