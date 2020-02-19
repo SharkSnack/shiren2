cb35b5e066183707c0f678056e2e5083558d46c3
SharkSnack <sharksnack2@gmail.com>
2020-02-19 09:55:56 -0600

    use more semantic html tags

--- js/dungeon.js
+++ js/dungeon.js
@@ -10,9 +10,6 @@ $(document).ready(function() {
     $("#sidebar").load("../../html/sidebar.html", function() {
       console.log('info - loaded sidebar');
     });
-    $("#footer").load("../../html/footer.html", function() {
-      console.log('info - loaded footer');
-    });
   } catch(e) {
     console.log('error: ' + e);
   }
--- js/item.js
+++ js/item.js
@@ -21,9 +21,6 @@ $(document).ready(function() {
     $("#sidebar").load("../../html/sidebar.html", function() {
       console.log('info - loaded sidebar');
     });
-    $("#footer").load("../../html/footer.html", function() {
-      console.log('info - loaded footer');
-    });
   } catch(e) {
     console.log('error: ' + e);
   }
